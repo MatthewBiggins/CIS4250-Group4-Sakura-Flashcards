ecc6f42fd0be97c6788d69c5a3b48ee1dce09a14
Matthew Biggins <mbiggins@uoguelph.ca>
2025-03-16 11:28:56 -0400

    more merge stuff

--- sakura-flashcards/app/studysets/[studySetId]/page.tsx
+++ sakura-flashcards/app/studysets/[studySetId]/page.tsx
@@ -28,13 +28,9 @@ export default function StudySet({ params }: StudySetProps) {
         </div>
 
         <div className="space-y-5">
-<<<<<<< HEAD
-          <h2 className="font-semibold uppercase tracking-wider">Navigation</h2>
-=======
-          <h2 className="font-semibold uppercase tracking-wider text-neutral-400">
+          <h2 className="font-semibold uppercase tracking-wider">
             Navigation
           </h2>
->>>>>>> main
           <ul className="space-y-2">
             {studySet.data.map((lesson) => (
               // list all the lessons
@@ -51,13 +47,9 @@ export default function StudySet({ params }: StudySetProps) {
         </div>
 
         <div className="space-y-5">
-<<<<<<< HEAD
-          <h2 className="font-semibold uppercase tracking-wider">Lessons</h2>
-=======
-          <h2 className="font-semibold uppercase tracking-wider text-neutral-400">
+          <h2 className="font-semibold uppercase tracking-wider">
             Lessons
           </h2>
->>>>>>> main
           <section className="space-y-10">
             {studySet?.data.map((lesson) => (
               // Display details about the lessons
@@ -70,13 +62,9 @@ export default function StudySet({ params }: StudySetProps) {
                   <h3 className="text-xl sm:text-2xl font-bold">
                     Lesson {lesson.lessonNumber}: {lesson.lessonTitle}
                   </h3>
-<<<<<<< HEAD
-                  <p className="font-semibold">({lesson.lessonPages})</p>
-=======
-                  <p className="text-neutral-400 font-semibold">
+                  <p className="font-semibold">
                     ({lesson.lessonPages})
                   </p>
->>>>>>> main
                 </div>
 
                 <div className="grid grid-cols-1 gap-2 sm:gap-5 lg:grid-cols-2">
@@ -87,21 +75,15 @@ export default function StudySet({ params }: StudySetProps) {
                       href={`/studysets/${studySet.slug}/${lesson.slug}/${unit.slug}`}
                       className="block space-y-1.5 rounded-lg px-5 py-3 bg-lessonLink hover:bg-lessonLink-hover custom-transition"
                     >
-<<<<<<< HEAD
-                      <h4 className="text-lessonColour text-lg sm:text-xl font-bold">{unit.title}</h4>
-
-                      {<p className="text-lessonColour line-clamp-3 font-semibold">{unit.items.length} Terms</p>}
-=======
-                      <h4 className="text-lg sm:text-xl font-bold">
+                      <h4 className="text-lessonColour text-lg sm:text-xl font-bold">
                         {unit.title}
                       </h4>
 
                       {
-                        <p className="line-clamp-3 text-neutral-400 font-semibold">
+                        <p className="text-lessonColour line-clamp-3 font-semibold">
                           {unit.items.length} Terms
                         </p>
                       }
->>>>>>> main
                     </Link>
                   ))}
                 </div>
